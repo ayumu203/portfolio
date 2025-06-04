2918f6186b1dd2aae495ea7a76eb6c53fbf0879a
arm203 <ochiohita@gmail.com>
2025-06-05 02:05:30 +0900

    fix:vercel用の修正2

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // よくわからんが,google fontとターボパックの相性悪い
-  experimental: {
-    turbo: false
-  }
+  // experimental: {
+  //   turbo: false
+  // }
 };
 
 export default nextConfig;
